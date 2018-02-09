e5c2b74e5be69f1d3cc6875978dd374b6363a8e3
tankdevin <32757674+tankdevin@users.noreply.github.com>
2018-02-09 22:13:33 +0700

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '103.90.136.206',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'frame',
     // 用户名
-    'username'        => 'xin.guo',
+    'username'        => 'mysql_user',
     // 密码
-    'password'        => '123456@a',
+    'password'        => 'mima@a',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
